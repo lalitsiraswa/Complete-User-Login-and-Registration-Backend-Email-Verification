b43c3439b76b7de4d4fbbddb0426dedcea202a9b
lalit <lalit@lalits-MacBook-Pro.local>
2022-12-07 20:17:39 +0530

    Initial Project Setup

---
+++ LoginAndRegistrationByAmigosCode /src/main/java/com/learning/LoginAndRegistrationByAmigosCode/LoginAndRegistrationByAmigosCodeApplication.java
@@ -0,0 +1,13 @@
+package com.learning.LoginAndRegistrationByAmigosCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAndRegistrationByAmigosCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAndRegistrationByAmigosCodeApplication.class, args);
+	}
+
+}
---
+++ LoginAndRegistrationByAmigosCode /src/test/java/com/learning/LoginAndRegistrationByAmigosCode/LoginAndRegistrationByAmigosCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.LoginAndRegistrationByAmigosCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndRegistrationByAmigosCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
