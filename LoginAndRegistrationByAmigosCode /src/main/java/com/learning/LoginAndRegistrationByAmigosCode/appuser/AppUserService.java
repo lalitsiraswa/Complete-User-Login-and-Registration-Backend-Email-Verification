1f6025f62fb9f4d6736992e1363d466ac3d9e558
lalit <lalit@lalits-MacBook-Pro.local>
2022-12-08 22:29:17 +0530

    Added Registration Service and Storing the AppUser in DataBase

--- LoginAndRegistrationByAmigosCode /src/main/java/com/learning/LoginAndRegistrationByAmigosCode/appuser/AppUserService.java
+++ LoginAndRegistrationByAmigosCode /src/main/java/com/learning/LoginAndRegistrationByAmigosCode/appuser/AppUserService.java
@@ -3,6 +3,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,11 +11,22 @@ import org.springframework.stereotype.Service;
 public class AppUserService implements UserDetailsService {
     private final static String USER_NOTE_FOUND_MSG = "user with email %s not found!";
     private final AppUserRepository appUserRepository;
-
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return appUserRepository.findByEmail(email)
                 .orElseThrow(()->
                         new UsernameNotFoundException(String.format(USER_NOTE_FOUND_MSG, email)));
+    }
+    public String signUpUser(AppUser appUser){
+        boolean userExists = appUserRepository.findByEmail(appUser.getEmail()).isPresent();
+        if(userExists){
+            throw new IllegalStateException(String.format("Email: %s already exists!", appUser.getEmail()));
+        }
+        String encodedPassword = bCryptPasswordEncoder.encode(appUser.getPassword());
+        appUser.setPassword(encodedPassword);
+        appUserRepository.save(appUser);
+//      TODO: Send Conformation Token
+        return "User Registration Successfully!";
     }
 }
--- LoginAndRegistrationByAmigosCode /src/main/java/com/learning/LoginAndRegistrationByAmigosCode/registration/RegistrationService.java
+++ LoginAndRegistrationByAmigosCode /src/main/java/com/learning/LoginAndRegistrationByAmigosCode/registration/RegistrationService.java
@@ -1,10 +1,29 @@
 package com.learning.LoginAndRegistrationByAmigosCode.registration;
 
+import com.learning.LoginAndRegistrationByAmigosCode.appuser.AppUser;
+import com.learning.LoginAndRegistrationByAmigosCode.appuser.AppUserRole;
+import com.learning.LoginAndRegistrationByAmigosCode.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+    private final EmailValidator emailValidator;
+    private final AppUserService appUserService;
     public String register(RegistrationRequest request) {
-        return "Works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if(!isValidEmail){
+            throw new IllegalStateException(String.format("Email: %s not valid!", request.getEmail()));
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstname(),
+                        request.getLastname(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
